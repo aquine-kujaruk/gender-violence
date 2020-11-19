6003a442cb87265946cfbc94aaefdba556c8dbe2
aquine-kujaruk <aquine.kujaruk@gmail.com>
2020-11-19 14:41:58 -0500

    react router fix

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,8 @@
 import React, {useState} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './Navbar';
 import Menu from './Menu';
 import Home from './../pages/Home';
-import AnimatedSwitch from './AnimatedSwitch';
-import AnimatedRoute from './AnimatedRoute';
 
 const Container = () => {
 	const [isOpenMenu, setIsOpenMenu] = useState(false);
@@ -14,14 +12,14 @@ const Container = () => {
 			<Navbar open={isOpenMenu} setOpen={setIsOpenMenu} />
 			<Menu open={isOpenMenu} setOpen={setIsOpenMenu} />
 			<Router>
-				<AnimatedSwitch animationClassName="page-slide-enter" animationTimeout={300}>
-					<AnimatedRoute path="/otro">
+				<Switch>
+					<Route exact path="/otro">
 						<Home />
-					</AnimatedRoute>
-					<AnimatedRoute path="/">
+					</Route>
+					<Route exact path="/">
 						<Home />
-					</AnimatedRoute>
-				</AnimatedSwitch>
+					</Route>
+				</Switch>
 			</Router>
 		</div>
 	);
